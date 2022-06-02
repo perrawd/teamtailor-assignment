255fae156a2629bf9de580e35514b7775d4c2c58
Per Rawdin <pr222iw@student.lnu.se>
2022-06-02 19:53:00 +0200

    Add Container in App component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Container } from 'semantic-ui-react'
+import Header from './components/Header/Header.js'
+import Joblist from './components/Joblist/Joblist.js'
 
 const App = () => {
+  // if location === true, set location in url
+  const [filter, setFilter] = useState(null)
+
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Container text>
+        <Header setFilter={setFilter}/>
+        <Joblist filter={filter}/>
+      </Container>
     </div>
   )
 }
