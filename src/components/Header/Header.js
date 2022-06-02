fc0860d3f6b516c50d94cff42bdf45eeb658a4ae
Per Rawdin <pr222iw@student.lnu.se>
2022-06-02 19:52:04 +0200

    Add showFilter state in Header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button } from 'semantic-ui-react'
+import LocationFilter from '../LocationFilter/LocationFilter.js'
 
 const Header = () => {
+  const [showFilter, setShowFilter] = useState(false)
+  const handleClick = () => {
+    setShowFilter(!showFilter)
+  }
+
   return (
     <div>
       <h1>TeamTailor JobList 🚀</h1>
       <Button color='green'>List All Jobs</Button>
-      <Button color='blue' onClick={console.log('show filter')}>Filter</Button>
+      <Button color='blue' onClick={handleClick}>Filter</Button>
+      { showFilter && <LocationFilter/>}
     </div>
   )
 }
